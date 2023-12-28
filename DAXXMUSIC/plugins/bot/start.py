39538a99588250309b5193e346e169ea6e2a9b30
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2023-12-28 00:34:43 -1100

    Update start.py

--- DAXXMUSIC/plugins/bot/start.py
+++ DAXXMUSIC/plugins/bot/start.py
@@ -127,7 +127,7 @@ async def start_gp(client, message: Message, _):
     uptime = int(time.time() - _boot_)
     await message.reply_photo(
         random.choice(YUMI_PICS),
-        caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
+        caption=_["start_2"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
     return await add_served_chat(message.chat.id)
