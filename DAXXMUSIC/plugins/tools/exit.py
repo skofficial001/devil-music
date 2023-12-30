dbaec3dd0ef2dc6a1445f1638ab5159323d0b601
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2023-12-30 15:11:47 +0530

    Update exit.py

--- DAXXMUSIC/plugins/tools/exit.py
+++ DAXXMUSIC/plugins/tools/exit.py
@@ -4,15 +4,9 @@ from pyrogram import filters
 from DAXXMUSIC Import bot as app
 
 @app.on_chat_member _updated(filters.chat)
-
 def member_update_handler(client, update, user):
-
 chat_id = update.chat.id
-
 member_name = user.first name if user.first name else "Unknown User"
-
 message_text=fHey everyone, (member_name) just dropped the mic and left the group! Catch you on the flip side!
-
 inline_keyboard= InlineKeyboard Markup([[Inli nekeyboardButton("Close", callback_data="close")]])
-
 client.send_message(chat_id, message text, reply_markup-inline_keyboard)
