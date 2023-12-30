951c0d61b56a492166d1658fdb63bd88d7154c59
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2023-12-30 15:07:02 +0530

    Update exit.py

--- DAXXMUSIC/plugins/tools/exit.py
+++ DAXXMUSIC/plugins/tools/exit.py
@@ -9,7 +9,7 @@ def member_update_handler(client, update, user):
 
 chat_id = update.chat.id
 
-member_name = user.first name if user.first name else "Unknown User
+member_name = user.first name if user.first name else "Unknown User"
 
 message_text=fHey everyone, (member_name) just dropped the mic and left the group! Catch you on the flip side!
 
