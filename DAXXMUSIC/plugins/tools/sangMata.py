4c0dacd57ab548b55ae8dde7e24e87042a2762f0
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2024-01-15 00:55:46 +0530

    Update sangMata.py

--- DAXXMUSIC/plugins/tools/sangMata.py
+++ DAXXMUSIC/plugins/tools/sangMata.py
@@ -5,8 +5,8 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from pyrogram.raw.functions.messages import DeleteHistory
 
-from AarohiX import userbot as us, app
-from AarohiX.core.userbot import assistants
+from DAXXMUSIC import userbot as us, app
+from DAXXMUSIC.core.userbot import assistants
 
 @app.on_message(filters.command("sangmata"))
 async def sg(client: Client, message: Message):
