554d3ff8f4b380f3d0aa7059e59e5ff45a46167e
SK OFFICIAL <114320411+skofficial001@users.noreply.github.com>
2024-02-06 01:24:15 +0530

    Update wishcute.py

--- DAXXMUSIC/plugins/Yumi/wishcute.py
+++ DAXXMUSIC/plugins/Yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from DAXXMUSIC import app 
 
-SUPPORT_CHAT = "ALLTYPECC"
+SUPPORT_CHAT = "Late_Night_Chatters"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
